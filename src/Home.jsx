2b0ac4a24331598d0426ad9d2626139ede9977b3
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-01 14:06:10 +0530

    onclick fun

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,17 @@
 const Home = ()=>{
+
+    const handleClick = () => { 
+        alert('HELLO WORLD');
+    }
+    const handleClickAgain = (name) => {
+        alert("hello"+ name)
+    }
+
     return(
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain("Rakesh")}>Click me again</button>
         </div>
     );
 }
