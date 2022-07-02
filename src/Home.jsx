756fccdd7b1c86d46f12d17a083820aabd736ba1
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-02 12:00:56 +0530

    error handeling

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,26 +4,32 @@ import BlogList from "./Bloglist";
 const Home = ()=>{
     const [blogs, setBlog] = useState(null);
     const [loading, setLoading] = useState(true);
-    
+    const [error, setError] = useState(null);
 
-    const handleDelete = (id) => {
-        setBlog(blogs.filter(blog => blog.id !== id));
-    }  
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){    
+                    throw Error("could not fetch data for that resource ");
+                }
                 return res.json();
             })
             .then(data => {
                 setBlog(data);
                 setLoading(false);
+                setError(null);
             })
+            .catch(err => {
+                setLoading(false);
+                setError(err.message);
+            })    
         }, 1000);
     }, []);
     return(
 
         <div className="home">
+            { error && <div>{ error }</div> }
             { loading && <div>Loading...</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
