4352c725da82297bcba87fd7a8a88ab3a106bce8
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-02 12:47:32 +0530

    react route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Navbar from './navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,10 @@
 import { useState,useEffect } from "react";
 import BlogList from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = ()=>{
-    const [blogs, setBlog] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok){    
-                    throw Error("could not fetch data for that resource ");
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlog(data);
-                setLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                setLoading(false);
-                setError(err.message);
-            })    
-        }, 1000);
-    }, []);
+    const {data: blogs, loading, error} = useFetch('http://localhost:8000/blogs');
+    
     return(
 
         <div className="home">
